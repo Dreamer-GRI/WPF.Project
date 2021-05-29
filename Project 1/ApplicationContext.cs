52dfb97d042f956f9a244a613f4a353083e1eec8
WSR <roman.grishin.02@mail.ru>
2021-05-29 15:02:48 +0300

    Создание классов для БД

--- Project 1/ApplicationContext.cs
+++ Project 1/ApplicationContext.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel;
 
 namespace Project_1
 {
     class ApplicationContext : DbContext
     {
-
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Project 1/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    public class User
+    {
+        private string log;
+        private string pass;
+
+        public int Id { get; set; }
+
+        public string Log
+        {
+            get { return log; }
+            set
+            {
+                log = value;
+            }
+        }
+        public string Pass
+        {
+            get { return pass; }
+            set
+            {
+                pass = value;
+            }
+        }
+    }
+}
